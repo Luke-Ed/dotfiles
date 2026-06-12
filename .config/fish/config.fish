70db07a0a38ef99e3c4dc71512bc0681fb9d021e
Luke Edmondson <luke_ed@outlook.com>
2026-06-12 10:03:17 -0500

    Correctly looped over macos fish configs, and removed rancher desktop management

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -3,11 +3,9 @@ fish_add_path "$HOME/.local/bin"
 switch (uname)
   case Darwin
     set -l macos_fish_configs "$__fish_config_dir/os/macos"
-    source $macos_fish_configs/*.fish
+    for file in $macos_fish_configs/*.fish
+      source $file
+    end
   case Linux
-    source "$HOME/.config/fish/os/linux.fish"
+    source "$__fish_config_dir/os/linux.fish"
 end
-
-### MANAGED BY RANCHER DESKTOP START (DO NOT EDIT)
-set --export --prepend PATH "/Users/ledmondson/.rd/bin"
-### MANAGED BY RANCHER DESKTOP END (DO NOT EDIT)
